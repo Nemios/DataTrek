4cf1988cb968b9db1ac22caac9dd7a354b3e5672
Nemios <sbaudot@ensc.fr>
2025-01-26 19:24:34 +0100

    test, changement path .csv

--- Tuto/test.py
+++ Tuto/test.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r"..\Data\Raw\student-depression-dataset.csv")
+df = pd.read_csv(r"Data\Raw\student-depression-dataset.csv")
 
 
 """
