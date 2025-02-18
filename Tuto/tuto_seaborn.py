0359048fcb9d1076a967fc85bd89da6fcdef6d90
Nemios <sbaudot@ensc.fr>
2025-02-18 14:40:21 +0100

    files seaborn tuto

---
+++ Tuto/tuto_seaborn.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+###############################################################
+# 19/30 Seaborn Pairplot
+###############################################################
+# https://seaborn.pydata.org/index.html
+
+iris = pd.read_csv("Tuto\seaborn-data-master\iris.csv")
