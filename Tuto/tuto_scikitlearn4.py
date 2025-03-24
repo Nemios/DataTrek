28cc3bc5aeee9a78dc405ea69f763ab505b34ef4
Nemios <sbaudot@ensc.fr>
2025-03-24 21:23:22 +0100

    Ensemble Learning : BAGGING + BOOSTING + STACKING

--- Tuto/tuto_scikitlearn4.py
+++ Tuto/tuto_scikitlearn4.py
@@ -5,7 +5,19 @@ os.environ["OMP_NUM_THREADS"] = "1"
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn.datasets import make_blobs
+from sklearn.datasets import make_blobs, make_moons
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import (
+    VotingClassifier,
+    BaggingClassifier,
+    RandomForestClassifier,
+    AdaBoostClassifier,
+    GradientBoostingClassifier,
+    StackingClassifier,
+)
 
 ###############################################################
 # 24/30 SKLearn : Apprentissage non-supervisé
@@ -28,7 +40,7 @@ from sklearn.datasets import make_blobs
 # et on garde le cas où les distances finales aux centroids sont les plus faibles (ie inertia / min variance)
 
 # génération données
-X, y = make_blobs(n_features=100, centers=3, cluster_std=0.8)
+""" X, y = make_blobs(n_features=100, centers=3, cluster_std=0.8)
 plt.figure()
 plt.scatter(X[:, 0], X[:, 1])
 plt.show()
@@ -175,8 +187,87 @@ plt.figure()
 plt.imshow(X_recovered[0].reshape((8, 8)))
 plt.show()
 print(model.n_components_)
-
+ """
 # ATTENTION :
 # il faut standardiser les données avant d'utiliser PCA (StandardScaler)
 # PCA est normalement conçue pour var continues
 # PCA pas efficace sur datasets non linéaires => Alternatives : Manifold Learning (IsoMap, T-SNE)
+
+###############################################################
+# 25/30 ENSEMBLE LEARNING : BAGGING, BOOSTING et STACKING
+###############################################################
+# theory : the wisdom of the crowd
+# basée sur loi des grands nombres, plus on a de modèle plus la performance collective est proche de 100%
+# ATTENTION : 2 critères à respecter
+# 1. les modeles ont individuellement au moins 50% de performance sinon convergence collective vers 0%
+# 2. il faut de la diversité entre les modèles sinon autant demander à un seul modèle, les faiblesses des uns
+
+# Pour Ensemble Learning, on dispose de 3 techniques :
+# BAGGING / BOOSTING / STACKING
+
+# BAGGING = créer plusieurs copies d'un mm modele en entrainant chaque copie sur partie aléatoire dataset
+# on utilise unz forme d'échantillonnage appelée BOOTSTRAPPING afin d'avoir des copies du modele qui se recoupent et se completent en faveur des bonnes réponses
+# un exemple de BAGGING est la random forest qui fait des copies d'arbre de décision
+# = entrainement en parallele de modeles forts ie overfitting mais reduit par rassemblement en un modele final
+
+# BOOSTING = entrainer l'un apres l'autre plusieurs modele faible en demandant au modele n+1 de corriger les erreurs du modele n
+# pour cela 2 algos existent : AdaBoost et Gradient Boosting
+# = entrainement en série de modeles faibles ie underfitting mais reduit par rassemblement en un modele final
+
+# STACKING = entrainer un modele de ML par dessus les predictions de la foule
+# au lieu de rassembler les resultats, on entraine un modele qui determine qui a raison qui a tort dans la foule pour lui meme predire
+# le resultat
+
+###############################################################
+# dans sklearn.ensemble avec datasets make_moons
+
+X, y = make_moons(n_samples=500, noise=0.3, random_state=0)
+
+plt.figure()
+plt.scatter(X[:, 0], X[:, 1], c=y)
+plt.show()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# Voting Classifier
+model_1 = SGDClassifier(random_state=0)
+model_2 = DecisionTreeClassifier(random_state=0)
+model_3 = KNeighborsClassifier(n_neighbors=2)
+
+model_4 = VotingClassifier(
+    [("SGD", model_1), ("Tree", model_2), ("KNN", model_3)], voting="hard"
+)
+# voting= Hard Voting : vote sur les predictions on prend la majoritaire ou Soft Voting : sur les probabilités de chaque classe
+
+for model in (model_1, model_2, model_3, model_4):
+    model.fit(X_train, y_train)
+    print(model.__class__.__name__, model.score(X_test, y_test))
+
+# ATTENTION : pas très bon niveau diversité
+# => diversité++ avec BAGGING et BOOSTING
+
+# BAGGING
+model = BaggingClassifier(estimator=KNeighborsClassifier(), n_estimators=100)
+
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+
+# Random Forest
+# en général meilleur que baggingclassifier mais à paramétrer
+model = RandomForestClassifier(n_estimators=100)
+# pas besoin de preciser estimator car estimator par defaut arbre de decision
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+
+# BOOSTING
+model = AdaBoostClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+
+# STACKING
+model_5 = StackingClassifier(
+    [("SGD", model_1), ("Tree", model_2), ("KNN", model_3)],
+    final_estimator=KNeighborsClassifier(),
+)
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
